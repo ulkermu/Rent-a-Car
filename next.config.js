654016e7b2b6476c71a73a344a81b94c084dafa5
ulkermu <84866808+ulkermu@users.noreply.github.com>
2023-05-01 06:17:01 +0300

    nextconfig

--- next.config.js
+++ next.config.js
@@ -7,34 +7,34 @@ const nextConfig = {
     defaultLocale: "tr",
     localeDetection: false,
   },
-  async rewrites() {
-    return [
-      // {
-      //   source: "/hakkimizda",
-      //   destination: "/hakkimizda",
-      // },
-      {
-        source: "/about",
-        destination: "/hakkimizda",
-      },
-      {
-        source: "/ueber-uns",
-        destination: "/hakkimizda",
-      },
-      // {
-      //   source: "/arac-kirala",
-      //   destination: "/arac-kirala",
-      // },
-      {
-        source: "/rent-a-car",
-        destination: "/arac-kirala",
-      },
-      {
-        source: "/ein-auto-mieten",
-        destination: "/arac-kirala",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     // {
+  //     //   source: "/hakkimizda",
+  //     //   destination: "/hakkimizda",
+  //     // },
+  //     {
+  //       source: "/about",
+  //       destination: "/hakkimizda",
+  //     },
+  //     {
+  //       source: "/ueber-uns",
+  //       destination: "/hakkimizda",
+  //     },
+  //     // {
+  //     //   source: "/arac-kirala",
+  //     //   destination: "/arac-kirala",
+  //     // },
+  //     {
+  //       source: "/rent-a-car",
+  //       destination: "/arac-kirala",
+  //     },
+  //     {
+  //       source: "/ein-auto-mieten",
+  //       destination: "/arac-kirala",
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
